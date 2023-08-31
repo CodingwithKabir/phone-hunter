736bf4f15ec26949c426bb7ba4ee7b4e045999dd
Kabir <kabir.du17@gmail.com>
2023-08-31 07:59:22 +0600

    card added

--- Script/phone.js
+++ Script/phone.js
@@ -3,6 +3,28 @@ const loadPhone = async () => {
     const data = await response.json();
     const phone = data.data;
     console.log(phone);
+    displayPhone(phone);
+}
+
+const displayPhone = phones => {
+    const phoneContainer = document.getElementById('card-container');
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList = `card w-96 bg-gray-100 shadow-xl`
+        phoneDiv.innerHTML = `
+        <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        phoneContainer.appendChild(phoneDiv);
+    })
 }
 
 loadPhone();
