b1d1b688f5d15aa987206acf620a77992214b1a2
Kabir <kabir.du17@gmail.com>
2023-08-31 20:42:40 +0600

    products add

--- Script/phone.js
+++ Script/phone.js
@@ -1,16 +1,28 @@
-const loadPhone = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchPhone) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchPhone}`);
     const data = await response.json();
     const phone = data.data;
-    console.log(phone);
+    // console.log(phone);
     displayPhone(phone);
 }
 
 const displayPhone = phones => {
     const phoneContainer = document.getElementById('card-container');
+    phoneContainer.textContent = '';
+
+    const showAllButton = document.getElementById('Show-all-container')
+    if(phones.length > 9){
+        showAllButton.classList.remove('hidden');
+    }
+    else{
+        showAllButton.classList.add('hidden');
+    }
+
+
+    phones = phones.slice(0, 9);
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
-        phoneDiv.classList = `card w-96 bg-gray-100 shadow-xl`
+        phoneDiv.classList = `card bg-gray-100 shadow-xl`
         phoneDiv.innerHTML = `
         <figure class="px-10 pt-10">
             <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
@@ -27,4 +39,10 @@ const displayPhone = phones => {
     })
 }
 
-loadPhone();
+const handleSearch = () => {
+    const searchField = document.getElementById('search-Field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
+
+// loadPhone();
